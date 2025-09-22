81f7ecb74f74947a40141bb00899d6075281c6ba
Lucia <Lucia.hernandezmontero@alumnos.upm.es>
2025-09-22 13:06:21 +0200

    Start the project

---
+++ src/main/java/es/upm/etsisi/poo/App.java
@@ -0,0 +1,20 @@
+package es.upm.etsisi.poo;
+
+
+public class App {
+    public static void main(String[] args) {
+        App app = new App();
+        app.init();
+        app.start();
+        app.end();
+    }
+
+    private void init() {
+    }
+
+    private void start() {
+    }
+
+    private void end() {
+    }
+}
