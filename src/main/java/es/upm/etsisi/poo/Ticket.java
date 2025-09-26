c17d28325a9af0e8ad38d6b954fe47902fa1394e
oscarLencero <oscar.lencero@alumnos.upm.es>
2025-09-26 10:28:04 +0200

    constructor del ticket

--- src/main/java/es/upm/etsisi/poo/Ticket.java
+++ src/main/java/es/upm/etsisi/poo/Ticket.java
@@ -6,7 +6,8 @@ public class Ticket {
     private static int MAX_CART_PRODUCTS = 100;
 
     public Ticket(){
-
+        this.cart = new Product[MAX_CART_PRODUCTS];
+        this.totalPrice = 0.0;
     }
     public void AddProduct(Product producto){
 
