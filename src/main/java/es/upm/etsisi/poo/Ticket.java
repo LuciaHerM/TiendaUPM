999268b5fefd37eb80a579e536755b5f8c837ce9
sserrgiiooo <sergio.carrascopardo@alumnos.upm.es>
2025-09-26 10:25:43 +0200

    Nombres de métodos y variables privadas

--- src/main/java/es/upm/etsisi/poo/Ticket.java
+++ src/main/java/es/upm/etsisi/poo/Ticket.java
@@ -1,4 +1,23 @@
 package es.upm.etsisi.poo;
 
 public class Ticket {
+    private Product[] cart;
+    Double totalPrice;
+    private static int MAX_CART_PRODUCTS = 100;
+
+    public Ticket(){
+
+    }
+    public void AddProduct(Product producto){
+
+    }
+    public void RemoveProduct(Product producto){
+
+    }
+    public void RemoveAll(){
+
+    }
+    public String ToString(){
+     return null;
+    }
 }
