f7531bc94b3d2a16df6405ab1d743593a6301c56
sserrgiiooo <sergio.carrascopardo@alumnos.upm.es>
2025-09-26 10:51:51 +0200

    Comentarios añadidos

--- src/main/java/es/upm/etsisi/poo/Ticket.java
+++ src/main/java/es/upm/etsisi/poo/Ticket.java
@@ -7,15 +7,21 @@ public class Ticket {
     private int productNumber;
     private Double totalDiscount;
 
+//Constructor de la clase ticket, que asigna valores a las variables privadas.
     public Ticket(){
         this.cart = new Product[MAX_CART_PRODUCTS];
         this.totalPrice = 0.0;
         this.productNumber = 0;
+        this.totalDiscount = 0.0;
     }
-    public void AddProduct(Product producto){
+//Añade el producto pasado por parámetro en la posición del total de productos
+//(debido a que empiezan los arrays en 0)
+    public void AddProduct(Product producto) {
         cart[productNumber] = producto;
         productNumber++;
     }
+//Elimina el producto pasado por parámetro.
+//Recoloca los productos por si queda un hueco en el array
     public void RemoveProduct(Product producto){
         boolean find = false;
         for(int i = 0 ; i < productNumber ; i++) {
@@ -30,9 +36,11 @@ public class Ticket {
             productNumber--;
         }
     }
+//Utiliza un bucle para eliminar los productos desde
+// el último producto hasta la posición 0.
     public void RemoveAll(){
         for (int i = productNumber - 1; i > 0; i--) {
-            RemoveProduct(cart[i]);
+            cart[i] = null;
         }
     }
     public String ToString(){
