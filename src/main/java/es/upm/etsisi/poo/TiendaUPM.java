6f3a159ad3c808c328755a4efefdf8460a0ad034
Lucia <Lucia.hernandezmontero@alumnos.upm.es>
2025-09-26 10:07:42 +0200

    Crear las clases y cambiar el nombre de la clase principal

--- src/main/java/es/upm/etsisi/poo/TiendaUPM.java
+++ src/main/java/es/upm/etsisi/poo/TiendaUPM.java
@@ -3,11 +3,11 @@ package es.upm.etsisi.poo;
 
 import java.util.Scanner;
 
-public class App {
+public class TiendaUPM {
 
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        App app = new App();
+        TiendaUPM app = new TiendaUPM();
         app.init();
         app.start();
         app.end();
