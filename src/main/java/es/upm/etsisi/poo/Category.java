007e8ddcd434702738aebb1637f8de9855612b23
Lucia <Lucia.hernandezmontero@alumnos.upm.es>
2025-10-31 09:07:52 +0100

    Añadimos los descuentos a las categorias

--- src/main/java/es/upm/etsisi/poo/Category.java
+++ src/main/java/es/upm/etsisi/poo/Category.java
@@ -1,9 +1,19 @@
 package es.upm.etsisi.poo;
 
 public enum Category {
-    MERCH,
-    STATIONERY,
-    CLOTHES,
-    BOOK,
-    ELECTRONICS
+    MERCH(0),
+    STATIONERY(5),
+    CLOTHES(7),
+    BOOK(10),
+    ELECTRONICS(3);
+
+    private final int descuento; // Porcentaje de descuento
+
+    Category(int descuento) {
+        this.descuento = descuento;
+    }
+
+    public int getDescuento() {
+        return descuento;
+    }
 }
