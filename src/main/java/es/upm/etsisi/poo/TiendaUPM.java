ac725fd8d69436b61775dce106dbd0bc997251b0
oscarLencero <oscar.lencero@alumnos.upm.es>
2025-10-31 10:30:06 +0100

    añadir cash

--- src/main/java/es/upm/etsisi/poo/TiendaUPM.java
+++ src/main/java/es/upm/etsisi/poo/TiendaUPM.java
@@ -256,15 +256,15 @@ public class TiendaUPM {
     private void cashAdd(String name, String email, String id){
         boolean encontrado = false;
         int i = 0 ;
-        while (!encontrado&&i<cashList.size()) {
-            if (cashList.get(i).getId().equals(id)) {
+        while (!encontrado&&i<cashers.size()) {
+            if (cashers.get(i).getId().equals(id)) {
                encontrado = true;
             }
             i++;
         }
         if(!encontrado) {
             Cash cash = new Cash(id, name, email);
-            cashList.add(cash);
+            cashers.add(cash);
         }else {
             System.err.println("Ya existe un cajero con el mismo Id en la base de datos");
         }
