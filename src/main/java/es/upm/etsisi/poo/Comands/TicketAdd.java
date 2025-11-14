d0221ac350cc4112970877274d9031634b1f8599
Paula Marina <p.marina@alumnos.upm.es>
2025-11-14 10:07:51 +0100

    Arreglar los comandos de tickets

--- src/main/java/es/upm/etsisi/poo/Comands/TicketAdd.java
+++ src/main/java/es/upm/etsisi/poo/Comands/TicketAdd.java
@@ -4,13 +4,12 @@ import es.upm.etsisi.poo.Ticket;
 import es.upm.etsisi.poo.Catalog;
 
 public class TicketAdd extends ComandTicket{
-    @Override
     /**
      *  Añade una cantidad específica de un producto al ticket.
      * @param prodId    Identificador del producto.
      * @param quantity  Cantidad de unidades a agregar.
      */
-    public void apply(String prodId, String quantity) {
+    public void apply(String prodId, String quantity, Catalog catalog, Ticket ticketActive) {
         int cont = 0;
         boolean encontrado = false;
         while (cont < catalog.length() && !encontrado){
@@ -25,7 +24,7 @@ public class TicketAdd extends ComandTicket{
             for (int i = 0; i < Integer.parseInt(quantity); i++) {
                 ticketActive.AddProduct(catalog.find(cont));
             }
-            ticketPrint();
+            System.out.println(ticketActive.toString());
             System.out.println("ticket add: ok");
         } else {
             System.err.println("The product was not found");
