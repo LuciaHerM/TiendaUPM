b5dfcbc980512b447d39e288cff772d661871f45
rosalia-lopez <rosalia.lopez@alumnos.upm.es>
2025-11-22 11:03:15 +0100

    Clase TicketNew

--- src/main/java/es/upm/etsisi/poo/Comands/TicketNew.java
+++ src/main/java/es/upm/etsisi/poo/Comands/TicketNew.java
@@ -9,46 +9,49 @@ import java.util.ArrayList;
 
 public  class TicketNew extends ComandTicket{
     private Catalog catalog;
-    private Ticket ticketActive;
     private String cashId;
     private ArrayList<Cash> cashers;
     private ArrayList<Client> clients;
+    private String clientId;
+
     public TicketNew(String cashId, String clientId, Catalog catalog, ArrayList<Cash> cashers, ArrayList<Client> clients, Ticket ticketActive){
         this.cashId=cashId;
         this.cashers=cashers;
         this.clients=clients;
+        this.clientId = clientId;
         this.catalog=catalog;
-        this.ticketActive=ticketActive;
     }
     /**
      * Le pasan por parametros el id del cajero y el dni del cliente al que pertenece el ticket
-     * ls cuales los busca en la lista de cajeros y clientes y si los encuentra crea un ticket con esos parametros .
+     * a los cuales busca en la lista de cajeros y clientes y si los encuentra crea un ticket con esos parametros.
      */
     public void apply() {
-        Cash cajero = null;
-        Client cliente = null;
+        Cash cash = null;
+        Client client = null;
 
         for(int i = 0 ; i < cashers.size();i++){
-            if(cashId.equals(cashers.get(i).getId())){
-                cajero=cashers.get(i);
+            Cash ca = cashers.get(i);
+            if(ca.getId().equals(cashId)){
+                cash= ca;
             }
         }
         for(int i = 0 ; i < clients.size();i++){
-            if(cashId.equals(clients.get(i).getDNI())){
-                cliente=clients.get(i);
+            Client c = clients.get(i);
+            if(c.getDNI().equals(clientId)){
+                client = c;
             }
         }
-        if(cajero!=null){
-            if(cliente!=null){
-                ticketActive=new Ticket(cajero,cliente);
-                System.out.println("ticket new: ok");
-            }
-            else {
-                System.err.println("El dni del cliente introducido no se encuentra en nuestra base de datos .");
-            }
+
+        if(cash == null) {
+            System.err.println("El id del cajero no se encuntra en nuestra base de datos.");
         }
-        else {
-            System.err.println("El id del cajero no se encuntra en nuestra base de datos .");
+        if(client == null) {
+            System.err.println("El dni del cliente introducido no se encuentra en nuestra base de datos.");
         }
+        // Ticket no gurarda al cajero ni el cliente
+        Ticket ticket = new Ticket();
+        client.ticketAddClients(ticket);
+        cash.ticketAddCash(ticket);
+        System.out.println("ticket new: ok");
     }
 }
