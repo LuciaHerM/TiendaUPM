7f04f58a662edd224e5f81a6b23ded53dbc1695a
Paula Marina <p.marina@alumnos.upm.es>
2025-12-18 10:03:23 +0100

    crear clase de producto basico, persanalizaciones y cambios en la clase productos

---
+++ src/main/java/es/upm/etsisi/poo/Personalized.java
@@ -0,0 +1,84 @@
+package es.upm.etsisi.poo;
+
+import java.util.Arrays;
+
+public class Personalized extends Product_Basic{
+
+    public int max_pers=0;
+    public String[] personalizaciones;
+
+    public Personalized(String id, String name, Category category, Double price, int max_pers) {
+        super(id,name,category,price);
+        this.personalizaciones=null;
+        if(max_pers>0){
+            this.max_pers=max_pers;
+        }else {
+            System.out.println("It's not possible to insert that maximon personalization. ");
+        }
+    }
+
+    public Personalized() {
+        super();
+    }
+
+    public String[] getPersonalizaciones() {
+        return personalizaciones;
+    }
+
+    public void setPersonalizaciones(String[] personalizaciones) {
+        if(max_pers > 0){
+            if(personalizaciones.length<=max_pers){
+                this.personalizaciones = personalizaciones;
+                this.price += this.price*0.1* personalizaciones.length;
+            }
+            else {
+                System.out.println("no puedes introducir esa cantidad de personalizaciones .");
+            }
+        }
+        else {
+            System.out.println("no puedes introducir personalizaciones a un producto no personalizable .");
+        }
+    }
+    public String personalizacionesToString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < personalizaciones.length; i++) {
+            sb.append(personalizaciones[i]);
+            if (i < personalizaciones.length - 1) {
+                sb.append(", ");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        if (personalizaciones==null){
+            return " {class:ProductPersonalized, id:" + id + ", name:'" + name + "', category:" + category + ", price:" + price + ", maxPersonal:" + max_pers + "}";
+        }
+        else {
+            return " {class:ProductPersonalized, id:" + id + ", name:'" + name + "', category:" + category + ", price:" + price + ", maxPersonal:" + max_pers + ", personalizationList:[" + personalizacionesToString()+ "]}";
+        }
+    }
+
+
+    @Override
+    public Product_Basic clone() {
+        Product_Basic copia = new Personalized();
+        copia.id = this.id;
+        copia.name = this.name;
+        copia.category = this.category;
+        copia.price = this.price;
+        copia.max_pers = this.max_pers;
+        if (this.personalizaciones != null) {
+            copia.personalizaciones = new String[this.personalizaciones.length];
+            for (int i = 0; i < this.personalizaciones.length; i++) {
+                copia.personalizaciones[i] = this.personalizaciones[i];
+            }
+        } else {
+            copia.personalizaciones = null;
+        }
+
+        return copia;
+    }
+}
