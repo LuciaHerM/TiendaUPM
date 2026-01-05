a869e33afb687a1555dd34043d45c2e8bb7dc06e
Lucia <Lucia.hernandezmontero@alumnos.upm.es>
2026-01-05 17:31:33 +0100

    Arreglando cosas

--- src/main/java/es/upm/etsisi/poo/Comands/ComandHelp.java
+++ src/main/java/es/upm/etsisi/poo/Comands/ComandHelp.java
@@ -5,19 +5,19 @@ public class ComandHelp extends Comands{
     @Override
     public void apply() {
         System.out.println("Commands:\n" +
-                "  client add \"<nombre>\" <DNI> <email> <cashId>\n" +
+                "  client add \"<nombre>\" (<DNI>|<NIF>) <email> <cashId>\n" +
                 "  client remove <DNI>\n" +
                 "  client list\n" +
                 "  cash add [<id>] \"<nombre>\"<email>\n" +
                 "  cash remove <id>\n" +
                 "  cash list\n" +
                 "  cash tickets <id>\n" +
-                "  ticket new [<id>] <cashId> <userId>\n" +
+                "  ticket new [<id>] <cashId> <userId> -[c|p|s] (default -p option)\n" +
                 "  ticket add <ticketId><cashId> <prodId> <amount> [--p<txt> --p<txt>] \n" +
                 "  ticket remove <ticketId><cashId> <prodId> \n" +
                 "  ticket print <ticketId> <cashId> \n" +
                 "  ticket list\n" +
-                "  prod add <id> \"<name>\" <category> <price>\n" +
+                "  prod add <id> \"<name>\" <category> <price> [<maxPers>]) || (\"<name>\" <category> )\n" +
                 "  prod update <id> NAME|CATEGORY|PRICE <value>\n" +
                 "  prod addFood [<id>] \"<name>\" <price> <expiration:yyyy-MM-dd> <max_people>\n" +
                 "  prod addMeeting [<id>] \"<name>\" <price> <expiration:yyyy-MM-dd> <max_people>\n" +
